04c4ede43c2a01f8beff052d22a092b74e95e239
Andrew Duong <itsandrewduong@gmail.com>
2022-09-27 15:25:42 -0700

    revised User model to fix Validator and added User signup backend endpoint

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,29 +1,28 @@
 'use strict';
+const { Model, Validator } = require('sequelize');
+
 const bcrypt = require('bcryptjs');
-const {
-  Model, Validator
-} = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     //Define an instance method toSafeObject in the user.js model file. This method will return an object with only the User instance information that is safe to save to a JWT, like id, username, and email.
     toSafeObject() {
       const { id, username, email } = this; // context will be the User instance
       return { id, username, email };
     }
+
     //Define an instance method validatePassword in the user.js model file. It should accept a password string and return true if there is a match with the User instance's hashedPassword. If there is no match, it should return false.
     validatePassword(password) {
       return bcrypt.compareSync(password, this.hashedPassword.toString());
     }
+
     //Define a static method getCurrentUserById in the user.js model file that accepts an id. It should use the currentUser scope to return a User with that id.
     static getCurrentUserById(id) {
       return User.scope("currentUser").findByPk(id);
     }
-    //Define a static method signup in the user.js model file that accepts an object with a username, email, and password key
+
+    //Define a static method login in the user.js model file. It should accept an object with credential and password keys. 
     static async login({ credential, password }) {
       const { Op } = require('sequelize');
       const user = await User.scope('loginUser').findOne({
@@ -39,9 +38,11 @@ module.exports = (sequelize, DataTypes) => {
       }
     }
     //Define a static method signup in the user.js model file that accepts an object with a username, email, and password key. Hash the password using the bcryptjs package's hashSync method. Create a User with the username, email, and hashedPassword. Return the created user using the currentUser scope.
-    static async signup({ username, email, password }) {
+    static async signup({ firstName, lastName, username, email, password }) {
       const hashedPassword = bcrypt.hashSync(password);
       const user = await User.create({
+        firstName,
+        lastName,
         username,
         email,
         hashedPassword
@@ -59,7 +60,7 @@ module.exports = (sequelize, DataTypes) => {
       validate: {
         len: [4, 30],
         isNotEmail(value) {
-          if (validator.isEmail(value)) {
+          if (Validator.isEmail(value)) {
             throw new Error('Cannot be an email.')
           }
         }
@@ -68,9 +69,10 @@ module.exports = (sequelize, DataTypes) => {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
       validate: {
         len: [3, 256],
-        isEmail: true
+        isEmail: true,
       }
     },
     hashedPassword: {

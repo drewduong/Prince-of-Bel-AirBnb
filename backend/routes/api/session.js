ba417c163a0687cec1b778ebf4756b4a5437a0f4
Andrew Duong <itsandrewduong@gmail.com>
2022-09-27 02:30:28 -0700

    Added User login backend endpoint, phase 4

---
+++ backend/routes/api/session.js
@@ -0,0 +1,33 @@
+// backend/routes/api/session.js
+const express = require('express')
+//In the backend/routes/api/session.js file, import the following code at the top of the file and create an Express router:
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+const router = express.Router();
+
+//Next, add the POST /api/session route to the router using an asynchronous route handler. 
+// Log in
+router.post(
+  '/',
+  async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.login({ credential, password });
+
+    if (!user) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
+      return next(err);
+    }
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user
+    });
+  }
+);
+
+module.exports = router;

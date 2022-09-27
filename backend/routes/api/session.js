7faa13ee9a9b8b76dc79b4a69e6f09ae581a43d0
Andrew Duong <itsandrewduong@gmail.com>
2022-09-27 16:03:39 -0700

    Added User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,6 +5,20 @@ const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+//middleware that will check the keys and validate
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
 
 // Restore session user
 router.get(
@@ -24,6 +38,7 @@ router.get(
 // Log in
 router.post(
   '/',
+  validateLogin,
   async (req, res, next) => {
     const { credential, password } = req.body;
 
@@ -44,7 +59,6 @@ router.post(
     });
   }
 );
-
 //The DELETE /api/session logout route will remove the token cookie from the response and return a JSON success message.
 // Log out
 router.delete(

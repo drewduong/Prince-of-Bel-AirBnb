fccbd3fa0bcd64dc070c89640be8d4397a1d7040
Andrew Duong <itsandrewduong@gmail.com>
2022-09-27 02:37:52 -0700

    Added User logout backend endpoint from phase 4

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -30,4 +30,14 @@ router.post(
   }
 );
 
+//The DELETE /api/session logout route will remove the token cookie from the response and return a JSON success message.
+// Log out
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
 module.exports = router;

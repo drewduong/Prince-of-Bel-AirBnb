fba3704be3d9c48f262ac6da93a55e6d87790d6b
Andrew Duong <itsandrewduong@gmail.com>
2022-10-02 02:48:25 -0700

    Testing creating an image for a review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -6,7 +6,45 @@ const sequelize = require('sequelize') //double check if we need this
 const { requireAuth } = require('../../utils/auth')
 
 //import model needed
-const { User, Spot, SpotImage, Review } = require('../../db/models')
+const { User, Spot, SpotImage, Review, ReviewImage } = require('../../db/models')
+
+// Add an image to a review based on the review's id
+router.post('/:reviewId/images', requireAuth, async (req, res) => {
+  const { url } = req.body
+  const review = await Review.findByPk(req.params.reviewId)
+
+  if (!review) {
+    res.status(404)
+    return res.json({
+      "message": "Review couldn't be found",
+      "statusCode": 404
+    })
+  }
+
+  const images = await ReviewImage.count({
+    where: {
+      reviewId: review.id
+    }
+  })
+
+  if (images === 10) {
+    res.status(403)
+    return res.json({
+      "message": "Maximum number of images for this resource was reached",
+      "statusCode": 403
+    })
+  }
+
+  const newImage = await ReviewImage.create({
+    url: url
+  })
+  res.status(200)
+  return res.json({
+    id: newImage.id,
+    url: newImage.url
+  })
+})
+
 
 
 

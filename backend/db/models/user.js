475f05141a7212ade7aae690df527562ad183f3a
Andrew Duong <itsandrewduong@gmail.com>
2022-10-03 03:30:46 -0700

    Changed associations

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -52,7 +52,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Spot, { foreignKey: 'ownerId' })
-      User.belongsToMany(models.Spot, { through: models.Booking })
+      User.belongsToMany(models.Spot, { through: models.Booking, foreignKey: 'userId' })
       User.hasMany(models.Review, { foreignKey: 'userId' })
     }
   }
